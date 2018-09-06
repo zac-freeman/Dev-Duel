f7b754d43ffed0baef83f790b0518d7f1847c954
Quinton Bolt <qbolt@cooksys.com>
2018-09-06 09:14:43 -0500

    ported from ft-curriculum

---
+++ server/routes/index.js
@@ -0,0 +1,49 @@
+import { Router } from 'express'
+import axios from 'axios'
+import validate from 'express-validation'
+
+import validation from './validation'
+
+export default () => {
+  let router = Router()
+
+  /** GET /health-check - Check service health */
+  router.get('/health-check', (req, res) => res.send('OK'))
+
+  /** GET /api/user/:username - Get user */
+  router.get('/user/:username', validate(validation.user), (req, res) => {
+    const username = req.param.username
+    /*
+      TODO
+      Fetch data for user specified in path variable
+      parse/map data to appropriate structure and return as json
+    */
+
+    // Example request for fetching and logging results from the first user in the query array
+    axios.get(`http://api.github.com/users/${username}`)
+      .then(res => res.json())
+      .then(data => console.log(data))
+
+    // The following is an example response using the express res.json() function
+    // This doesn't model the required response object and is only used for validating the endpoint exists
+    return res.json({ username: 'example-user' })
+  })
+
+  /** GET /api/users?username - Get users */
+  router.get('/users/', validate(validation.users), (req, res) => {
+    /*
+      TODO
+      Fetch data for users specified in query
+      parse/map data to appropriate structure and return as json
+
+      If req.query.usernames is defined but not an array, that means the client only specified one user
+      For convenience to the users of our API, if the client only specified one user, we will fetch that user like we do in /user/:userId
+    */
+
+    // The following is an example response using the express res.json() function
+    // This doesn't model the required response object and is only used for validating the endpoint exist
+    return res.json([{ username: 'example-user-1' }, { username: 'example-user-2' }])
+  })
+
+  return router
+}
