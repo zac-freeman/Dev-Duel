11fb30aab5a8b95fb8a14c322ea410eacdcb1774
unknown <zpfreeman14@gmail.com>
2018-09-06 14:07:10 -0500

    finished implementing titles in createProfile

--- server/service/profile.js
+++ server/service/profile.js
@@ -1,5 +1,25 @@
 export const createProfile = (userData, userRepos) => {
-  let reposData = userRepos.reduce(
+  let reposData = parseReposData(userRepos)
+
+  return {
+    username: userData.login,
+    name: userData.name,
+    location: userData.location,
+    bio: userData.bio,
+    avatar_url: userData.avatar_url,
+    titles: decreeTitles(userData, reposData),
+    favorite_language: findPropertyOfMax(reposData.languages),
+    public_repos: userData.public_repos,
+    total_stars: reposData.total_stars,
+    highest_starred: reposData.highest_starred,
+    perfect_repos: reposData.perfect_repos,
+    followers: userData.followers,
+    following: userData.following
+  }
+}
+
+const parseReposData = repos =>
+  repos.reduce(
     (acc, current) => {
       acc.total_stars += current.stargazers_count
       acc.highest_starred = Math.max(
@@ -7,6 +27,7 @@ export const createProfile = (userData, userRepos) => {
         current.stargazers_count
       )
       acc.perfect_repos += current.open_issues_count ? 0 : 1
+      acc.forks += current.fork
       acc.languages[current.language] = acc.languages[current.language]
         ? ++acc.languages[current.language]
         : 1
@@ -16,25 +37,30 @@ export const createProfile = (userData, userRepos) => {
       total_stars: 0,
       highest_starred: 0,
       perfect_repos: 0,
+      forks: 0,
       languages: {}
     }
   )
 
-  return {
-    username: userData.login,
-    name: userData.name,
-    location: userData.location,
-    bio: userData.bio,
-    avatar_url: userData.avatar_url,
-    titles: '[todo, ...]',
-    favorite_language: findPropertyOfMax(reposData.languages),
-    public_repos: userData.public_repos,
-    total_stars: reposData.total_stars,
-    highest_starred: reposData.highest_starred,
-    perfect_repos: reposData.perfect_repos,
-    followers: userData.followers,
-    following: userData.following
+const decreeTitles = (userData, reposData) => {
+  let titles = []
+  if (reposData.forks * 2 >= userData.public_repos) {
+    titles.push('Forker')
   }
+  if (Object.keys(reposData.languages).length === 1) {
+    titles.push('One Trick Pony')
+  } else if (Object.keys(reposData.languages).length >= 10) {
+    titles.push('Jack of All Trades')
+  }
+  if (userData.following >= userData.followers * 2) {
+    titles.push('Stalker')
+  } else if (userData.followers >= userData.following * 2) {
+    titles.push('Mr. Popular')
+  }
+  if (reposData.highest_starred * 2 >= reposData.total_stars) {
+    titles.push('One Hit Wonder')
+  }
+  return titles
 }
 
 const findPropertyOfMax = obj =>
