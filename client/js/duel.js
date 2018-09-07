82617debf6126fbf19fe38ee720672bfa982f5a8
unknown <zpfreeman14@gmail.com>
2018-09-06 23:11:34 -0500

    finished duel site

--- client/js/duel.js
+++ client/js/duel.js
@@ -4,13 +4,29 @@ $('form').submit(() => {
   const leftUsername = $('form input[name="username-left"]').val()
   const rightUsername = $('form input[name="username-right"]').val()
 
-  // fetch(`${USER_URL}?username=${usernames[0]}&username=${usernames[1]}`)
-  //   .then(response => response.json())
-  //   .then(profiles => {
-  //     profiles.map(profile => postProfile(profile))
-  //     $('.duel-container').removeClass('hide')
-  //   })
+  fetch(`${USERS_URL}?username=${leftUsername}&username=${rightUsername}`)
+    .then(response => response.json())
+    .then(profiles => {
+      postProfile(profiles[0], 'left')
+      postProfile(profiles[1], 'right')
+      $('.duel-container').removeClass('hide')
+    })
+
+  return false
 })
+
+const postProfile = (profile, side) => {
+  for (let prop in profile) {
+    $(`.${side} span.${prop}`).removeClass('hide')
+    if (profile[prop] === null) {
+      $(`.${side} span.${prop}`).addClass('hide')
+    }
+
+    $(`.${side} span.${prop}`).text(profile[prop])
+  }
+
+  $(`.${side} img.avatar`).attr('src', profile['avatar'])
+}
 
 /*
   TODO
