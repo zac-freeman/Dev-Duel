9f49d996b3a69aa4cadd540395c9d092e9ecd1a0
unknown <zpfreeman14@gmail.com>
2018-09-07 13:57:06 -0500

    added good enough error handling to duel page

--- server/routes/validation/users.js
+++ server/routes/validation/users.js
@@ -2,6 +2,15 @@ import Joi from 'joi'
 
 export default {
   query: {
-    username: Joi.array().items(Joi.string()).single().required()
+    username: Joi.array()
+      .single()
+      .required()
+      .items(
+        Joi.string()
+          .regex(/^[a-zA-Z0-9][a-zA-Z0-9-]*$/)
+          .required()
+          .min(1)
+          .max(39)
+      )
   }
 }
