e50c19ecae288061848ce0696f486c1178e86dee
unknown <zpfreeman14@gmail.com>
2018-09-06 23:13:27 -0500

    helpful comments

--- client/js/duel.js
+++ client/js/duel.js
@@ -28,12 +28,4 @@ const postProfile = (profile, side) => {
   $(`.${side} img.avatar`).attr('src', profile['avatar'])
 }
 
-/*
-  TODO
-  Fetch 2 user's github data and display their profiles side by side
-  If there is an error in finding user or both users, display appropriate error
-  message stating which user(s) doesn't exist
-
-  It is up to the student to choose how to determine a 'winner'
-  and displaying their profile/stats comparison in a way that signifies who won.
- */
+// TODO: determine winner, handle nonexistent users
